e52b2652cac3e8baec2d9bf8229eb8fd09b1a448
vipcxj <vipcxj@126.com>
2018-03-23 13:29:25 +0800

    feature: support withRef option of withRouter

--- src/withRouter.js
+++ src/withRouter.js
@@ -1,13 +1,29 @@
 /* eslint-disable react/require-default-props,react/no-children-prop */
+import invariant from 'invariant';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Route from 'react-router/Route';
 import hoistStatics from 'hoist-non-react-statics';
 
-const withRouter4Compat = (Component) => {
+const withRouter4Compat = (Component, { withRef = false } = {}) => {
   class C extends React.Component {
+    // noinspection JSUnusedGlobalSymbols
+    getWrappedInstance() {
+      invariant(
+        withRef,
+        'To access the wrapped instance, you need to specify ' +
+        '`{ withRef: true}` as the second argument of the withRouter() call.',
+      );
+      return this.wrappedInstance;
+    }
     render() {
       const { wrappedComponentRef, ...remainingProps } = this.props;
+      const wrappedComponentRefCompat = wrappedComponentRef ? (c) => {
+        this.wrappedInstance = c;
+        return wrappedComponentRef(c);
+      } : (c) => {
+        this.wrappedInstance = c;
+      };
       return (
         <Route
           children={(routeComponentProps) => {
@@ -19,7 +35,7 @@ const withRouter4Compat = (Component) => {
               routes={this.context.routes}
               params={match.params}
               location={location}
-              ref={wrappedComponentRef}
+              ref={wrappedComponentRefCompat}
             />);
           }}
         />
